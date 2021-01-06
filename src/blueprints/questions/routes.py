2b5bb3d6e80c0b89f8990e5abe5445d79b4eb46a
Shannen <shannen_dolls@live.nl>
2021-01-06 15:17:32 +0100

    added basic endpage

--- src/blueprints/questions/routes.py
+++ src/blueprints/questions/routes.py
@@ -13,7 +13,10 @@ def question(question_id):
         session_id = case.id
 
     question_order = [1,3,2]
-    question = Question.get_by_id(question_order[question_id])    
+    if question_order[question_id]:
+        question = Question.get_by_id(question_order[question_id])
+    else:
+        return redirect(url_for('questions.advice'))            
     formtype = QuestionType.query().filter_by(name=Question.questiontype).first().name
     form = get_form(formtype)
     form.answer.label.text = question.question
@@ -27,5 +30,23 @@ def question(question_id):
         answer = form.answer.data
         Answer.create(answer=answer, answeredquestion=question.id, case=session_id)
 
+        if question_order[-1] == question_id:
+            return redirect(url_for('questions.advice'))
+
         return redirect(url_for('questions.question', question_id=question_id+1))
     return render_template('form.html', form=form)
+
+
+# Route for advice page
+@bp.route('/advice', methods=['GET', 'POST'])
+def advice():
+    # Checks whether there is a session_id, otherwise the user gets redirected
+    # to the startpage
+    session_id = request.cookies.get('sessionID')
+    if not session_id:
+        redirect(url_for('main.index'))
+
+    # Renders the recommendation page and resets the session_id in the cookie
+    resp = make_response(render_template('advice.html'))
+    resp.set_cookie('sessionID', '', expires=0)
+    return resp
