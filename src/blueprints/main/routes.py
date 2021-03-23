e84483f1087840cbc16d364c69b41ffee9bf233e
Eddy van den Aker <eddy.v.d.aker@gmail.com>
2021-03-23 14:31:14 +0100

    UI changes

--- src/blueprints/main/routes.py
+++ src/blueprints/main/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from src.blueprints.main import bp
 
 
 @bp.route('/')
 def index():
+    return redirect(url_for("questions.start"))
     return render_template('index.html')
